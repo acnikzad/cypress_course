d7a90768b954e78954005004f3ab5e0f9f07947f
Aria Nikzad <a.c.nikzad@gmail.com>
2022-12-08 10:19:46 -0800

    added hello world test

--- cypress/e2e/mocha-demo.cy.ts
+++ cypress/e2e/mocha-demo.cy.ts
@@ -3,13 +3,19 @@ describe('empty spec', () => {
     before(()=> {
         cy.log('hello from before the hook')
     });
+    after(()=> {
+        cy.log('hello from after the hook')
+    });
+    beforeEach(()=> {
+        cy.log('hello from beforeEach the hook')
+    });
     it('testcase #1', () => {
       cy.log("Hello World");
     });
-    it('testcase #2', () => {
+    it.skip('testcase #2', () => {
         cy.log("Hello World");
     });
-    it('testcase #3', () => {
+    it.only('testcase #3', () => {
         cy.log("Hello World");
     });
   })
