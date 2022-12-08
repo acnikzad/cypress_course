6f7e82c520faf02ec5cbc4e4a8f9a46815b4bb70
Aria Nikzad <a.c.nikzad@gmail.com>
2022-12-08 13:11:47 -0800

    added click to test and verified outcome

--- cypress/e2e/visit.cy.ts
+++ cypress/e2e/visit.cy.ts
@@ -15,4 +15,9 @@ describe('basics', () => {
             expect(title).to.be.equal("Text Input")
         })
     });
+
+    it("Input Challenge", () => {
+        cy.get('input#newButtonName').type('Hello from Input');
+        cy.get('button#updatingButton').click().should('have.text', 'Hello from Input');
+    })
   })
