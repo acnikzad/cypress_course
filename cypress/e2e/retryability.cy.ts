b415902b01863c8bb330d5d2676bdb82eafb6d99
Aria Nikzad <a.c.nikzad@gmail.com>
2022-12-09 08:31:41 -0800

    added progress bar test

--- cypress/e2e/retryability.cy.ts
+++ cypress/e2e/retryability.cy.ts
@@ -8,4 +8,10 @@ describe('Retry ability demo', () => {
         cy.get('#ajaxButton').click(0);
         cy.get('.bg-success').should('have.text', 'Data calculated on the client side.');
     });
+
+    it.only("progress bar scenario", () => {
+        cy.visit("/progressbar");
+        cy.get("#startButton").click();
+        cy.get("#progressBar", {timeout:30000}).should("have.text", "100%");
+    });
 });
